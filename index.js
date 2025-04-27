0e3bdf3a9fb22e64ead1e6e0cc36a87dc1caebb2
fathimahabeeb222 <134144650+fathimahabeeb222@users.noreply.github.com>
2025-04-27 20:38:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+document.querySelector("body").addEventListener("keypress", roll);
+
+function roll(){
+    
+    let randomNumber1 = Math.floor(Math.random()*6)+1;
+    document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+    let randomNumber2 = Math.floor(Math.random()*6)+1;
+    document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+    if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+    }
+    else if(randomNumber1<randomNumber2)
+    {
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+    }
+    else
+    {
+        document.querySelector("h1").innerHTML="Draw";
+    }    
+
+}
+
